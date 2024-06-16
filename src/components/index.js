3a7840e1d5847912b45c3c1709a6c7d4a44af1e4
acharayaP03 <acharyaP03@gmail.com>
2024-06-17 06:01:47 +1000

    create progress bar ui

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Header from './Header';
 import StartQuizz from './StartQuizz';
 import Question from './Question';
 import AnswerList from './AnswerList';
+import Progress from './Progress';
 
-export { Header, StartQuizz, Question, AnswerList };
+export { Header, StartQuizz, Question, AnswerList, Progress };
