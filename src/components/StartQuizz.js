2fd6b194583d2ba982b06b5377446d81e199d750
acharayaP03 <acharyaP03@gmail.com>
2024-06-17 05:27:17 +1000

    index to export all

--- src/components/StartQuizz.js
+++ src/components/StartQuizz.js
@@ -1,9 +1,11 @@
-function StartQuizz() {
+function StartQuizz({ totalQuestions, dispatch }) {
 	return (
 		<div className='start'>
 			<h2>Welcome to the Let's Quizz!</h2>
-			<h3>X questions to test your quizz mastery</h3>
-			<button className='btn btn-ui'>Let's start</button>
+			<h3>{totalQuestions} questions to test your quizz mastery</h3>
+			<button className='btn btn-ui' onClick={() => dispatch({ type: 'start' })}>
+				Let's start
+			</button>
 		</div>
 	);
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import Header from './Header';
 import StartQuizz from './StartQuizz';
+import Question from './Question';
+import AnswerList from './AnswerList';
 
-export { Header, StartQuizz };
+export { Header, StartQuizz, Question, AnswerList };
